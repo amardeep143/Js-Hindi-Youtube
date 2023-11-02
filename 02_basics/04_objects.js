a0756a5d9e6d6a22cb2330a38f04bbbbbe1e5a72
amardeep143 <112235091+amardeep143@users.noreply.github.com>
2023-11-02 07:07:11 +0000

    Json_Api

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,29 @@
+const course={
+    coursname:"Js chai aur code",
+    price:"999",
+    courseInstructor:"Hitesh"
+}
+
+const {courseInstructor:instructor }=course
+
+console.log(instructor)
+
+//++++++++++++++++++++++++++++++++++_____API_____++++++++++++++++++++++++++++++++++++++++++++++++
+
+// {
+//     "name":"Hitesh"
+//     "price":"free"
+//     "courceName":"Chai Aur Code"
+
+// }
+
+
+[
+    {},
+    {},
+    {}
+]
+
+
+
+
