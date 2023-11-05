3dad0e9d8c8cac538e6f73fcd97894c7b99b6399
amardeep143 <112235091+amardeep143@users.noreply.github.com>
2023-11-05 13:25:35 +0000

    Flow Control

---
+++ Practice_Js/if_else_elseif.js
@@ -0,0 +1,21 @@
+let age=17;
+
+if(age>=18){
+
+ //   console.log("You are eligible for vote")
+
+}else{
+    //console.log("You are not eligiblefor vote")
+}
+
+let num1 = 10;
+let num2 = 20;
+let num3 = 30;
+
+if (num1 >= num2 && num1 >= num3) {
+    console.log(`${num1} is greater than ${num2} and ${num3}`);
+} else if (num2 >= num1 && num2 >= num3) {
+    console.log(`${num2} is greater than ${num1} and ${num3}`);
+} else {
+    console.log(`${num3} is greater than ${num1} and ${num2}`);
+}
